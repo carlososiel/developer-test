ad0389a2ba65c394dd3e96d08361438b0cb14cf0
Adrian Arencibia Herrera <aaherrera@uci.cu>
2018-01-09 18:58:38 -0500

    [ADD] category validations

--- routers/category.js
+++ routers/category.js
@@ -1,20 +1,33 @@
 var express = require('express');
 var router = express.Router();
+var Joi = require('joi');
 var category = require('../models/category');
+var categoryValidator = require("../validations/category")
 
 //CRUD Category
-router.route('/').post(function (req, res) {  
-    var c = new category();
-    c.name = req.body.name;
-    console.log("--- "+ req.body);
-    console.log("--- "+ req.body.name);
-    c.save(function (err) {
-        if (err) {
-            res.send(err);
-        }
-        console.log("added");
-        res.send({ message: 'Category Created !' })
-    })
+router.route('/').post(function (req, res) {
+
+    const ret = Joi.validate(req.body, categoryValidator, {
+        // return an error if body has an unrecognised property
+        allowUnknown: false,
+        // return all errors a payload contains, not just the first one Joi finds
+        abortEarly: false
+    });
+
+    if (ret.error) {
+        res.status(400).end(ret.error.toString());
+    } else {
+
+        var c = new category();
+        c.name = req.body.name;
+        c.save(function (err) {
+            if (err) {
+                res.send(err);
+            }
+            console.log("added");
+            res.send({ message: 'Category Created !' })
+        })
+    }
 });
 
 router.route('/').get(function (req, res) {
@@ -22,41 +35,53 @@ router.route('/').get(function (req, res) {
         if (err) {
             res.send(err);
         }
-        
+
         res.send(categories)
     })
 });
 
 router.route('/:category_id').get(function (req, res) {
-    category.findById(req.params.category_id,function (err, category) {
+    category.findById(req.params.category_id, function (err, category) {
         if (err) {
             res.send(err);
-        }        
+        }
         res.send(category)
     })
 });
 
 router.route('/:category_id').put(function (req, res) {
-    category.findById(req.params.category_id,function (err, categ) {
-        if (err) {
-            res.send(err);
-        }
-        categ.name = req.body.name;      
-        categ.save(function (err) {
+
+    const ret = Joi.validate(req.body, categoryValidator, {
+        // return an error if body has an unrecognised property
+        allowUnknown: false,
+        // return all errors a payload contains, not just the first one Joi finds
+        abortEarly: false
+    });
+
+    if (ret.error) {
+        res.status(400).end(ret.error.toString());
+    } else {
+        category.findById(req.params.category_id, function (err, category) {
             if (err) {
                 res.send(err);
-            }            
-            res.send(categ)
-        })         
-    })
+            }
+            category.name = req.body.name;
+            category.save(function (err) {
+                if (err) {
+                    res.send(err);
+                }
+                res.send(category)
+            })
+        })
+    }
 });
 
 router.route('/:category_id').delete(function (req, res) {
-    category.findByIdAndRemove(req.params.category_id,function (err, categ) {
+    category.findByIdAndRemove(req.params.category_id, function (err, category) {
         if (err) {
             res.send(err);
         }
-        res.json({ message: 'Successfully deleted' });       
+        res.json({ message: 'Successfully deleted' });
     })
 });
 module.exports = router;
