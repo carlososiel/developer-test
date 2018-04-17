2e49dbfcd69eabf1043f17e79dbcd608879494ff
Unknown <manytostao@gmail.com>
2018-04-17 12:44:47 -0400

    Create generic service for crud operations

--- bookstore/client/src/app/_services/crud.service.ts
+++ bookstore/client/src/app/_services/crud.service.ts
@@ -15,6 +15,22 @@ export abstract class CrudService {
     return this.http.get(this.url + this.model);
   }
 
+  getById(id: string) {
+    return this.http.get(this.url + this.model + '/' + id);
+  }
+
+  update(model: any) {
+    return this.http.patch(this.url + this.model + '/' + model._id, model);
+  }
+
+  delete(id: string) {
+    return this.http.delete(this.url + this.model + '/' + id);
+  }
+
+  save(model: any) {
+    return this.http.post(this.url + this.model, model);
+  }
+
   getDisplayedColumns(): string[] {
     return undefined;
   }
